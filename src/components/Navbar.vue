fc011653d3de6bcfa93058be0b67166a532e6ac2
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-08-15 14:52:56 -0300

    feat: add mobile da página sobre

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,17 @@
 <template>
   
-<nav class="bg-primary-base fixed w-full z-20 top-0 left-0 h-20">
+<nav class="bg-primary-base fixed w-full z-20 top-0 left-0 sm:h-20 shadow-md">
   <div class="flex flex-wrap justify-between items-center mx-auto">
-    <div class="bg-black flex-none bg-opacity-20 w-1/4 py-6 flex justify-center">
+    <div class="sm:bg-black flex-none sm:bg-opacity-20 sm:w-1/4 w-2/4 sm:h-20 py-5 px-5 flex justify-center">
         <img src="@/assets/logo.svg" alt="Perfil Político" />
     </div>
-    <div class="flex flex-none md:order-2 px-2 sm:px-6 py-2.5">
+    <div class="flex sm:flex-none md:order-2 px-2 sm:px-6 py-2.5">
         <div class="c-footer__content-link-project flex items-center p-3 rounded-lg">
             <img src="@/assets/icons/icon-github.svg" alt="Github" />
             <span><a href="http://" target="_blank" rel="noopener noreferrer" class="ml-2 text-white">Ver Github</a></span>
         </div>
     </div>
-    <div class="hidden w-3/5 grow md:flex md:order-1" id="navbar-sticky">
+    <div class="hidden sm:w-3/5 grow md:flex md:order-1" id="navbar-sticky">
         <div class="hidden relative md:block w-full">
         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <span class="material-symbols-outlined text-white">search</span>
