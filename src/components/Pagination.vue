70bd91246fc09897cca678e3e68086e84bd6789e
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-08-22 11:08:08 -0300

    feat:add componente de paginação

---
+++ src/components/Pagination.vue
@@ -0,0 +1,35 @@
+<template>
+<nav class="c-pagination" aria-label="Page navigation example">
+  <ul class="inline-flex items-center -space-x-px">
+    <li>
+      <a href="#" class="block p-3 ml-0 mr-2 leading-tight text-text-light rounded-full hover:bg-primary-base hover:text-white">
+        <span class="sr-only">Anterior</span>
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="block px-4 py-3 mr-2 leading-tight text-white bg-primary-base rounded-full hover:bg-primary-base">1</a>
+    </li>
+    <li>
+      <a href="#" class="block px-4 py-3 mr-2 leading-tight text-text-light rounded-full hover:bg-primary-base hover:text-white">2</a>
+    </li>
+    <li>
+      <a href="#" class="block p-3 leading-tight text-text-light rounded-full hover:bg-primary-base hover:text-white">
+        <span class="sr-only">Próximo</span>
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/CandidateList.vue
+++ src/views/CandidateList.vue
@@ -3,6 +3,7 @@
   <div class="candidate-list my-48">
     <div class="container">
       <Card />
+      <Pagination />
     </div> 
   </div>
 <Footer></Footer>
@@ -13,12 +14,14 @@ import { Options, Vue } from "vue-class-component";
 import Footer from "@/components/Footer.vue";
 import Navbar from "@/components/Navbar.vue";
 import Card from "@/components/Card.vue";
+import Pagination from "../components/Pagination.vue";
 
 @Options({
   components: {
     Footer,
     Navbar,
-    Card
+    Card,
+    Pagination
   },
 })
 export default class Home extends Vue {}
