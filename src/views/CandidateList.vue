1d1f50c43e423b4f04d6426a7d2db01a6f299a44
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-08-25 14:10:34 -0300

    feat: criando componente de card para informações e gráficos

--- src/views/CandidateList.vue
+++ src/views/CandidateList.vue
@@ -2,8 +2,9 @@
 <Navbar></Navbar>
   <div class="candidate-list my-48">
     <div class="container">
-      <Card />
+      <CardCandidate />
       <Pagination />
+      <CardInfo />
     </div> 
   </div>
 <Footer></Footer>
@@ -13,15 +14,17 @@
 import { Options, Vue } from "vue-class-component";
 import Footer from "@/components/Footer.vue";
 import Navbar from "@/components/Navbar.vue";
-import Card from "@/components/Card.vue";
-import Pagination from "../components/Pagination.vue";
+import CardCandidate from "@/components/CardCandidate.vue";
+import Pagination from "@/components/Pagination.vue";
+import CardInfo from "../components/CardInfo.vue";
 
 @Options({
   components: {
     Footer,
     Navbar,
-    Card,
-    Pagination
+    CardCandidate,
+    Pagination,
+    CardInfo
   },
 })
 export default class Home extends Vue {}
