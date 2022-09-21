fe71628e11c6b21610713f2a95091c48de5e542f
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-09-21 18:39:39 -0300

    chore: adicionando loading na página de listagem

--- src/views/Home/FormSearchCandidate.vue
+++ src/views/Home/FormSearchCandidate.vue
@@ -73,7 +73,9 @@
           <label class="relative block">
             <span class="sr-only">Digite o nome da pessoa candidata</span>
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-              <span class="material-symbols-outlined">search</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>              
             </span>
             <input
               type="text"
