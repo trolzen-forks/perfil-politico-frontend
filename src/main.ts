b64aa54e16fc375b17c3f6e96bd9b1be0df40450
Giulio Carvalho <giulioccavalcante@gmail.com>
2022-09-21 10:59:53 -0300

    Adiciona Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import "flowbite";
 import "animate.css";
 import "@/assets/styles/main.css";
 import "@/assets/styles/fonts.css";
-import VueApexCharts from "vue3-apexcharts";
 
-createApp(App).use(router).use(VueApexCharts).mount("#app");
+import VueApexCharts from "vue3-apexcharts";
+import VueGtag from "vue-gtag";
+
+createApp(App).use(router).use(VueApexCharts).use(VueGtag, {config: {id: 'G-W3TYTKSHVG'}}, router).mount("#app");
